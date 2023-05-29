df6d331162997870e36ad2aadb590cfe515f0d84
soheee94 <hsh8616@naver.com>
2023-05-29 15:54:50 +0900

    next13 - client/server component, loading

---
+++ packages/apps/src/app/content/TestComponent.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import * as React from 'react';
+import styled from '@emotion/styled';
+
+interface Props {
+  
+};
+const TestComponent = (props: Props) => {
+  return (
+    <Wrapper>
+        hello
+    </Wrapper>
+  );
+};
+export default TestComponent;
+
+const Wrapper = styled.div`
+  color:red;
+`
